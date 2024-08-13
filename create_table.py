97e41e79e859eb6374bec43ab12f06dad09192ea
Ben Parsons <23parsonsb830@collyers.ac.uk>
2024-08-13 23:10:36 +0100

    registation system working, saving to database, password hashing securly

--- create_table.py
+++ create_table.py
@@ -1,9 +1,20 @@
 import sqlite3
 
-conn = sqlite3.connect('database.db')
-print("Connected to database successfully")
 
-conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
-print("Table created successfully")
+conn = sqlite3.connect('database.db')
+#print("Connected to database successfully")
+
+conn.execute('DROP TABLE personal_details')
+conn.execute('DROP TABLE login_info')
+print("Dropped all tables successfully")
+
+conn.execute('CREATE TABLE personal_details (personalID INTEGER PRIMARY KEY ,first_name TEXT, last_name TEXT, address TEXT, city TEXT, email TEXT)')
+print("Personal Detail's table created successfully")
+
+conn.execute('CREATE TABLE login_info (userID INTEGER PRIMARY KEY, username TEXT, password TEXT, FOREIGN KEY(userID) REFERENCES personal_details(personalID))')
+print("Login Information's table created successfully")
+
+
 
 conn.close()
+print("Disconnected from database successfully")
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+from flask_bcrypt import Bcrypt 
 
-app = Flask (__name__)
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-@app.route('//<name>')
-@app.route('/home/<name>')
-def index(name):
-    return render_template("index.html", user = name)
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
 
 @app.route('/aboutus')
 def about():
@@ -27,6 +30,41 @@ def lessons():
 def tables():
     return render_template("tables.html")
 
+@app.route('/join', methods=['POST', 'GET'])
+def join():
+    if request.method == 'POST':
+        try:
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            address = request.form['address']
+            city = request.form['city']
+            email = request.form['email']
+            username = request.form['username']
+            password = request.form['password']
+
+            hashed_password = bcrypt.generate_password_hash (password).decode('utf-8') 
+            #is_valid = bcrypt.check_password_hash (hashed_password, password) 
+            #return f"Password: {password}<br>Hashed Password: {hashed_password}<br>Is Valid: {is_valid}"
 
 
-app.run(debug=True)
+            with sqlite3.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO personal_details (first_name, last_name , address, city, email) VALUES (?,?,?,?,?)", (first_name, last_name, address, city, email))
+                cur.execute("INSERT INTO login_info (username, password) VALUES (?,?)", (username, hashed_password))
+                con.commit()
+                msg = "Record successfully added"
+        except Exception as e:
+            con.rollback()
+            msg = f"Error in insert operation: {str(e)}"
+        finally:
+            con.close()
+            return render_template("result.html", msg=msg)
+    else:
+        return render_template("join.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
