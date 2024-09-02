e134ad1cd593439a9983d873280223d919b9bd54
Ben Parsons <23parsonsb830@collyers.ac.uk>
2024-09-02 22:46:44 +0100

    Improvements to the database and the creation python database file

--- create_table.py
+++ create_table.py
@@ -10,36 +10,136 @@ conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
 # Drop existing tables
-conn.execute('DROP TABLE IF EXISTS personal_details')
 conn.execute('DROP TABLE IF EXISTS login_info')
-conn.execute('DROP TABLE IF EXISTS tables_info')
-conn.execute('DROP TABLE IF EXISTS restaurants_info')
+conn.execute('DROP TABLE IF EXISTS personal_details')
+conn.execute('DROP TABLE IF EXISTS table_bookings')
+conn.execute('DROP TABLE IF EXISTS table_info')
+conn.execute('DROP TABLE IF EXISTS restaurant_info')
+conn.execute('DROP TABLE IF EXISTS lesson_bookings')
+conn.execute('DROP TABLE IF EXISTS instructor_expertise')
+conn.execute('DROP TABLE IF EXISTS lessons_info')
+conn.execute('DROP TABLE IF EXISTS type_of_lesson')
+conn.execute('DROP TABLE IF EXISTS instructor_info')
 print("Dropped all tables successfully")
 
 # Create tables
-conn.execute('''CREATE TABLE personal_details (personalID INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, address TEXT, city TEXT, email TEXT)''')
+conn.execute('''
+CREATE TABLE personal_details (
+    personalID INTEGER PRIMARY KEY,
+    firstName TEXT,
+    lastName TEXT,
+    address TEXT,
+    city TEXT
+)''')
 print("Personal Details table created successfully")
 
-conn.execute('''CREATE TABLE login_info (userID INTEGER PRIMARY KEY, username TEXT, password TEXT,FOREIGN KEY(userID) REFERENCES personal_details(personalID))''')
+conn.execute('''
+CREATE TABLE login_info (
+    userID INTEGER PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    personalID INTEGER,
+    adminRights BOOLEAN,
+    FOREIGN KEY(personalID) REFERENCES personal_details(personalID)
+)''')
 print("Login Information table created successfully")
 
-conn.execute('''CREATE TABLE restaurants_info (restaurantsID INTEGER PRIMARY KEY, phoneNumber TEXT, email TEXT, address TEXT, city TEXT)''')
-print("Restaurants Information table created successfully")
+conn.execute('''
+CREATE TABLE restaurant_info (
+    restaurantID INTEGER PRIMARY KEY,
+    phoneNumber TEXT,
+    email TEXT,
+    address TEXT,
+    city TEXT
+)''')
+print("Restaurant Information table created successfully")
 
 conn.execute('''
-CREATE TABLE tables_info (tableID INTEGER PRIMARY KEY, restaurantsID INTEGER, localTableNumber INTEGER, FOREIGN KEY(restaurantsID) REFERENCES restaurants_info(restaurantsID))''')
+CREATE TABLE table_info (
+    tableID INTEGER PRIMARY KEY,
+    restaurantID INTEGER,
+    localTableNumber INTEGER,
+    FOREIGN KEY(restaurantID) REFERENCES restaurant_info(restaurantID)
+)''')
 print("Table Information table created successfully")
+
+conn.execute('''
+CREATE TABLE table_bookings (
+    tableBookingID INTEGER PRIMARY KEY,
+    userID INTEGER,
+    numberOfPeople INTEGER,
+    tableID INTEGER,
+    bookingMade DATE,
+    DateFor DATE,
+    TimeFor TIME,
+    lessonID INTEGER,
+    FOREIGN KEY(userID) REFERENCES login_info(userID),
+    FOREIGN KEY(tableID) REFERENCES table_info(tableID),
+    FOREIGN KEY(lessonID) REFERENCES lessons_info(lessonID)
+)''')
+print("Table Bookings table created successfully")
+
+conn.execute('''
+CREATE TABLE lesson_bookings (
+    lessonBookingID INTEGER PRIMARY KEY,
+    bookingDateAndTime DATETIME,
+    userID INTEGER,
+    totalAmount REAL,
+    lessonID INTEGER,
+    FOREIGN KEY(userID) REFERENCES login_info(userID),
+    FOREIGN KEY(lessonID) REFERENCES lessons_info(lessonID)
+)''')
+print("Lesson Bookings table created successfully")
+
+conn.execute('''
+CREATE TABLE instructor_info (
+    instructorID INTEGER PRIMARY KEY,
+    personalID INTEGER,
+    FOREIGN KEY(personalID) REFERENCES personal_details(personalID)
+)''')
+print("Instructor Info table created successfully")
+
+conn.execute('''
+CREATE TABLE lessons_info (
+    lessonID INTEGER PRIMARY KEY,
+    instructorID INTEGER,
+    restaurantID INTEGER,
+    dateOfLesson DATE,
+    numberOfPlaces INTEGER,
+    lessonTypeID INTEGER,
+    FOREIGN KEY(instructorID) REFERENCES instructor_info(instructorID),
+    FOREIGN KEY(restaurantID) REFERENCES restaurant_info(restaurantID),
+    FOREIGN KEY(lessonTypeID) REFERENCES type_of_lesson(lessonTypeID)
+)''')
+print("Lessons Info table created successfully")
+
+conn.execute('''
+CREATE TABLE type_of_lesson (
+    lessonTypeID INTEGER PRIMARY KEY,
+    titleOfLesson TEXT,
+    costOfLesson REAL
+)''')
+print("Type of Lesson table created successfully")
+
+conn.execute('''
+CREATE TABLE instructor_expertise (
+    qualificationID INTEGER PRIMARY KEY,
+    instructorID INTEGER,
+    lessonTypeID INTEGER,
+    FOREIGN KEY(instructorID) REFERENCES instructor_info(instructorID),
+    FOREIGN KEY(lessonTypeID) REFERENCES type_of_lesson(lessonTypeID)
+)''')
+print("Instructor Expertise table created successfully")
 
 # Insert test data
 conn.execute('''
-INSERT INTO personal_details (first_name, last_name, address, city, email)
-VALUES ('Bob', 'Smith', '123 Main Street', 'Cardiff', 'bob.smith@email.com')
+INSERT INTO personal_details (firstName, lastName, address, city)
+VALUES ('Bob', 'Smith', '123 Main Street', 'Cardiff')
 ''')
 
 conn.execute('''
-INSERT INTO login_info (username, password)
-VALUES ('bob.smith', ?)
-''', (hashed_password,))
+INSERT INTO login_info (username, password, personalID, adminRights)
+VALUES ('bob.smith', ?, 1, 0)''', (hashed_password,))
 print("Added test data into login_info & personal_details tables successfully")
 
 # Commit the changes and close the connection
